dd17e8fd90665d774465c7938d59f5bee0bd6fb5
depytat88 <ziabko4488@gmail.com>
2020-02-25 00:53:40 +0300

    [1] first init app

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+
+// // server.js
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// // Run the app by serving the static files
+// // in the dist directory
+// app.use(express.static(__dirname + '/dist/sportEvents'));
+// // Start the app by listening on the default
+// // Heroku port
+// app.listen(process.env.PORT || 4200);
+//
+// app.get('/assets', function (req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/assets'));
+// });
+//
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
